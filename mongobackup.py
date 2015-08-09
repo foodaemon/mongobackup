05d7ac73194f93e5c983098d6ce507441787b0f2
Robin <nixdmn@gmail.com>
2015-08-09 11:13:09 -0500

    added function templates.

--- mongobackup.py
+++ mongobackup.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python3
+
 import os
+import argparse
+import datetime
+
 
 __author__ = 'Robin Tiwari'
 __version__ = '0.1.0'
@@ -6,8 +11,35 @@ __description__ = 'MongoDB backup and restore utility'
 __license__ = 'MIT'
 
 
+backup_path = os.getcwd()
+
+parser = argparse.ArgumentParser(description='backup/restore mongodb')
+parser.add_argument('-t', '--target-dir',
+                    type=str,
+                    required=False,
+                    default=None,
+                    help='target directory')
+parser.add_argument('-u', '--user',
+                    type=str,
+                    required=False,
+                    default=None,
+                    help='mongodb user')
+parser.add_argument('-p', '--password',
+                    type=str,
+                    required=False,
+                    default=None,
+                    help='mongodb password')
+parser.add_argument('--port',
+                    type=str,
+                    required=False,
+                    default=None,
+                    help='mongodb port')
+
+
 def backup():
-    pass
+    now = datetime.datetime.today().strftime('%Y%m%d-%H%M%S')
+    backup_name = os.path.join(backup_path, now)
+    os.mkdir(backup_name)
 
 
 def restore():
@@ -15,4 +47,5 @@ def restore():
 
 
 if __name__ == '__main__':
-    pass
+    args = parser.parse_args()
+
