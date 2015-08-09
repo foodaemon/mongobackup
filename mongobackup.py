a323ab89bdd12cbb36094f0fb1f2f7e4cb01afdc
Robin <nixdmn@gmail.com>
2015-08-09 13:55:07 -0500

    added function implementation

--- mongobackup.py
+++ mongobackup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import os
 import argparse
 import datetime
+import subprocess
 
 
 __author__ = 'Robin Tiwari'
@@ -11,24 +13,32 @@ __description__ = 'MongoDB backup and restore utility'
 __license__ = 'MIT'
 
 
-backup_path = os.getcwd()
-
 parser = argparse.ArgumentParser(description='backup/restore mongodb')
-parser.add_argument('-t', '--target-dir',
+
+parser.add_argument('-d', '--database',
                     type=str,
                     required=False,
                     default=None,
-                    help='target directory')
+                    help='database')
+
+parser.add_argument('-o', '--output',
+                    type=str,
+                    required=False,
+                    default=None,
+                    help='output directory')
+
 parser.add_argument('-u', '--user',
                     type=str,
                     required=False,
                     default=None,
                     help='mongodb user')
+
 parser.add_argument('-p', '--password',
                     type=str,
                     required=False,
                     default=None,
                     help='mongodb password')
+
 parser.add_argument('--port',
                     type=str,
                     required=False,
@@ -36,10 +46,26 @@ parser.add_argument('--port',
                     help='mongodb port')
 
 
-def backup():
+def backup(args):
+    backup_path = os.path.join(os.path.expanduser("~"), 'backup')
+    if args.output is not None:
+        backup_path = args.output
     now = datetime.datetime.today().strftime('%Y%m%d-%H%M%S')
     backup_name = os.path.join(backup_path, now)
     os.mkdir(backup_name)
+
+    if args.output is not None:
+        backup_path = args
+
+    stat = subprocess.check_output('mongodump',
+                                   '-host', 'localhost',
+                                   '-u', '%s' % args.user,
+                                   '-p', '%s' % args.password,
+                                   '-d', '%s' % args.database,
+                                   '--port', '%s' % args.port,
+                                   '-o', '%s' % backup_path)
+
+    print(stat)
 
 
 def restore():
@@ -48,4 +74,4 @@ def restore():
 
 if __name__ == '__main__':
     args = parser.parse_args()
-
+    backup(args)
